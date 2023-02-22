3a7744a2526f2fc61177ee515ae193fecaef1d96
Solaris50 <s4thapa@uwaterloo.ca>
2023-02-21 22:10:03 -0500

    Added the global css setup

--- client/src/components/Room/index.js
+++ client/src/components/Room/index.js
@@ -1,84 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import "./index.css";
 import Typography from "@material-ui/core/Typography";
-import Link from '@material-ui/core/Link';
-import history from '../Navigation/history';
+import Link from "@material-ui/core/Link";
+import history from "../Navigation/history";
 
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { MuiThemeProvider, createTheme } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 
-import AppBar from '@material-ui/core/AppBar';
-import Box from '@material-ui/core/Box';
-import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
+import AppBar from "@material-ui/core/AppBar";
+import Box from "@material-ui/core/Box";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
 
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../Firebase/firebase";
 
-import CustomAppBar from '../CustomAppBar';
-
-const opacityValue = 0.9;
-
-const theme = createTheme({
-  palette: {
-    type: 'light',
-    background: {
-      default: "#000000"
-    },
-    primary: {
-      main: "#7a1a06",
-    },
-    secondary: {
-      main: "#ffcb52",
-    },
-  },
-});
-
-const styles = theme => ({
-  root: {
-    body: {
-      backgroundColor: "#000000",
-      opacity: opacityValue,
-      overflow: "hidden",
-    },
-  },
-  mainMessage: {
-    opacity: opacityValue,
-  },
-
-  mainMessageContainer: {
-    marginTop: "15vh",
-    marginLeft: theme.spacing(20),
-    [theme.breakpoints.down('xs')]: {
-      marginLeft: theme.spacing(4),
-    },
-  },
-  paper: {
-    overflow: "hidden",
-  },
-  message: {
-    opacity: opacityValue,
-    maxWidth: 250,
-    paddingBottom: theme.spacing(2),
-  },
-
-});
+import CustomAppBar from "../CustomAppBar";
 
 const Room = (props) => {
-
   const [user, setUser] = useState({});
 
   const { classes } = props;
 
   onAuthStateChanged(auth, (currUser) => {
-      setUser(currUser);
+    setUser(currUser);
   });
 
   const mainMessage = (
     <Box sx={{ flexGrow: 1 }}>
-
       <CustomAppBar />
 
       <Grid
@@ -86,37 +38,31 @@ const Room = (props) => {
         spacing={0}
         direction="column"
         alignItems="flex-start"
-        style={{ minHeight: '100vh' }}
-        className={classes.mainMessageContainer}
+        style={{ minHeight: "100vh" }}
+        class="mainMessageContainer"
       >
         <Grid item>
-          <Typography
-            variant={"h3"}
-            className={classes.mainMessage}
-            align="flex-start"
-          >
+          <Typography variant={"h3"} class="mainMessage" align="flex-start">
             Room
           </Typography>
 
           <Typography variant="h6" color="inherit">
             Welcome to the Roomies App {user.email}!
           </Typography>
-
+          <Button variant="text">Text</Button>
         </Grid>
       </Grid>
     </Box>
   );
 
   return (
-    <MuiThemeProvider theme={theme}>
-      <div className={classes.root}>
+    <MuiThemeProvider>
+      <div>
         <CssBaseline />
-        <Paper className={classes.paper}>
-          {mainMessage}
-        </Paper>
+        <Paper class="paper">{mainMessage}</Paper>
       </div>
     </MuiThemeProvider>
   );
-}
+};
 
-export default withStyles(styles)(Room);
+export default Room;
