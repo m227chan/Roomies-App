9a7a875037db0ae2a52fa5fb3d6ccd83c26a84e4
zzammituw <zzammit@uwaterloo.ca>
2023-02-22 19:39:58 -0500

    Expense APIs Complete

--- server.js
+++ server.js
@@ -200,33 +200,52 @@ app.post('/api/getOwedByPerson', (req, res) => {
 app.post('/api/getExpenseReport', (req, res) => {
 
 	let connection = mysql.createConnection(config);
-	//Input: (Firebase ID, Sort Value (4 Character String see below), Filter)
-	//Output: Table of Transaction related to the user.
+	//8 Inputs: (Firebase ID, Sort Value (4 Character String see below), Start of Date Range, End of Date Range, Tag to filter by, 
+	//			Spender Firebase ID to filter by, Debtor Firebase ID to filter by, 
+	//			Whether or not you only want to look at the user's tracactions(Empty String = Yes, Anything Else = No))
+	//Output: Table of Transactions sorted and filtered.
 	let getExpensesSQL = `
 	SET @roomie := (Select id from zzammit.Roomate where firebaseUID = (?)); 
 	SET @orderV := (?);
-
+	
+	SET @DateStart := (?);
+	SET @DateEnd := (?);
+	SET @Tag := (?);
+	SET @Spender := (?);
+	SET @Debtor := (?);
+	SET @justUser := (?);
+	
 	DROP TABLE IF EXISTS zzammit.ExpLog;
-	CREATE TEMPORARY TABLE zzammit.ExpLog Select Expenses.id as ExpenseID, CONCAT(firstName, ' ', lastName) as Spender, idDebtor, amount, tag, comments, tdate 
-		From zzammit.Expenses left join zzammit.Roomate on Expenses.idSpender = Roomate.id where (idSpender = @roomie or idDebtor = @roomie);
-    
+	CREATE TEMPORARY TABLE zzammit.ExpLog Select Expenses.id as ExpenseID, CONCAT(firstName, ' ', lastName) as Spender, idDebtor, amount, tag, comments, tDate 
+		From zzammit.Expenses left join zzammit.Roomate on Expenses.idSpender = Roomate.id where 
+		CASE @justUser WHEN '' THEN (idSpender = @roomie or idDebtor = @roomie) ELSE idSpender IN 
+			(SELECT id FROM zzammit.Roomate WHERE idRoom = 
+				(SELECT idRoom FROM zzammit.Roomate WHERE id = @roomie)) END
+		;
+		
 	Select ExpenseID, Spender, CONCAT(Roomate.firstName, ' ', Roomate.lastName) as Debtor, amount, tDate, tag, comments 
 		From zzammit.ExpLog left join zzammit.Roomate on ExpLog.idDebtor = Roomate.id
-    	Order By 
-    	CASE @orderV WHEN 'TagA' THEN tag END ASC,
-    	CASE @orderV WHEN 'TagD' THEN tag END DESC,
-    	CASE @orderV WHEN 'SpdA' THEN Spender END ASC,
-    	CASE @orderV WHEN 'SpdD' THEN Spender END DESC,
-    	CASE @orderV WHEN 'DbtA' THEN Debtor END ASC,
-    	CASE @orderV WHEN 'DbtD' THEN Debtor END DESC,
-    	CASE @orderV WHEN 'AmtA' THEN amount END ASC,
-    	CASE @orderV WHEN 'AmtD' THEN amount END DESC,
-    	CASE @orderV WHEN 'DatA' THEN tDate END ASC,
-    	CASE @orderV WHEN 'DatD' THEN tDate END DESC;
-
+		WHERE
+			CASE @Tag WHEN '' THEN tDate >= '1900-01-01' ELSE tag = @Tag END AND
+			CASE @Spender WHEN '' THEN tDate >= '1900-01-01' ELSE Spender = (Select CONCAT(firstName, ' ', lastName) from zzammit.Roomate where firebaseUID = @Spender) END AND
+			CASE @Debtor WHEN '' THEN tDate >= '1900-01-01' ELSE idDebtor = (Select id from zzammit.Roomate where firebaseUID = @Debtor) END AND
+			CASE @DateStart WHEN '' THEN tDate >= '1900-01-01' ELSE tDate >= @DateStart END AND
+			CASE @DateEnd WHEN '' THEN tDate >= '1900-01-01' ELSE tDate <= @DateEnd END
+		Order By 
+		CASE @orderV WHEN 'TagA' THEN tag END ASC,
+		CASE @orderV WHEN 'TagD' THEN tag END DESC,
+		CASE @orderV WHEN 'SpdA' THEN Spender END ASC,
+		CASE @orderV WHEN 'SpdD' THEN Spender END DESC,
+		CASE @orderV WHEN 'DbtA' THEN Debtor END ASC,
+		CASE @orderV WHEN 'DbtD' THEN Debtor END DESC,
+		CASE @orderV WHEN 'AmtA' THEN amount END ASC,
+		CASE @orderV WHEN 'AmtD' THEN amount END DESC,
+		CASE @orderV WHEN 'DatA' THEN tDate END ASC,
+		CASE @orderV WHEN 'DatD' THEN tDate END DESC;
+	
 	DROP TABLE IF EXISTS zzammit.ExpLog;
 	`;
-	let getExpensesData = [req.body.user, req.body.sort, req.body.filter];
+	let getExpensesData = [req.body.user, req.body.sort, req.body.dateStart, req.body.dateEnd, req.body.tag, req.body.spenderID, req.body.debtorID, req.body.justUser];
 
 	// console.log(req.body);
 
@@ -277,7 +296,14 @@ app.post('/api/deleteExpense', (req, res) => {
 	//Input: Expense Trasaction ID
 	//Output: None
 	let delExpenseSQL = `
-	DELETE FROM zzammit.Expenses WHERE id = (?);
+	SET @expID = (?);
+	SET @oldAmt = (SELECT amount from zzammit.Expenses where id = @expID);
+	SET @oldS = (SELECT idSpender from zzammit.Expenses where id = @expID);
+	SET @oldD = (SELECT idDebtor from zzammit.Expenses where id = @expID);
+
+	Update zzammit.Roomate set owed = owed - @oldAmt where id = @oldS;
+	Update zzammit.Roomate set owed = owed + @oldAmt where id = @oldD;
+	DELETE FROM zzammit.Expenses WHERE id = @expID;
 	`;
 	let delExpenseData = [req.body.expenseID];
 
@@ -305,9 +331,39 @@ app.post('/api/editExpense', (req, res) => {
 	SET @amt = (?);
 	SET @spender = (Select id from zzammit.Roomate where firebaseUID = (?));
 	SET @debtor = (Select id from zzammit.Roomate where firebaseUID = (?));
+	SET @oldAmt = (SELECT amount from zzammit.Expenses where id = @expID);
+	SET @oldS = (SELECT idSpender from zzammit.Expenses where id = @expID);
+	SET @oldD = (SELECT idDebtor from zzammit.Expenses where id = @expID);
+
+	Update zzammit.Roomate set owed = owed - @oldAmt where id = @oldS;
+	Update zzammit.Roomate set owed = owed + @oldAmt where id = @oldD;
 	UPDATE zzammit.Expenses SET idSpender = @spender, idDebtor = @debtor, amount = @amt, tag = (?), comments = (?), tDate = (?) WHERE id = @expID;
-	Update zzammit.Roomate set owed = owed + @amt where id = @spendor;
+	Update zzammit.Roomate set owed = owed + @amt where id = @spender;
 	Update zzammit.Roomate set owed = owed - @amt where id = @debtor;
+	`;
+	let editExpenseData = [req.body.expenseID, req.body.amount, req.body.spender, req.body.debtor, req.body.tag, req.body.comment, req.body.date];
+
+	// console.log(req.body);
+
+	connection.query(editExpenseSQL, editExpenseData, (error, results, fields) => { 
+		if (error) {
+			console.log(error.message);
+		}
+
+		let string = JSON.stringify(results);
+		//let obj = JSON.parse(string);
+		res.send({ express: string });
+	});
+	connection.end();
+});
+
+app.post('/api/shortExchange', (req, res) => {
+
+	let connection = mysql.createConnection(config);
+	//Input: FireBase ID
+	//Output:
+	let editExpenseSQL = `
+	
 	`;
 	let editExpenseData = [req.body.expenseID, req.body.amount, req.body.spender, req.body.debtor, req.body.tag, req.body.comment, req.body.date];
 
