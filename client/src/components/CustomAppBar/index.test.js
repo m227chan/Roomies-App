cd4f30483dda5ee7f28ce3b8accbb71939debf63
m227chan <matthew.tc76@gmail.com>
2023-02-25 17:28:59 -0500

    test

--- client/src/components/CustomAppBar/index.test.js
+++ client/src/components/CustomAppBar/index.test.js
@@ -3,12 +3,10 @@ import renderer from 'react-test-renderer';
 import CustomAppBar from './index';
 
 describe('CustomAppBar', () => {
-
     it('renders correctly', () => {
         const tree = renderer
             .create(<CustomAppBar />)
             .toJSON();
         expect(tree).toMatchSnapshot();
     });
-
 });
