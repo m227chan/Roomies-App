d62bc1513c44a419ba1d58e8362c91a18f8ed523
m227chan <matthew.tc76@gmail.com>
2023-03-15 12:01:22 -0400

    fixed bug where expense table wouldn't load

--- client/src/components/Expenses/expenseTable.js
+++ client/src/components/Expenses/expenseTable.js
@@ -9,7 +9,6 @@ import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../Firebase/firebase";
-import "firebase/auth";
 
 const serverURL = "http://localhost:3000/"; //enable for dev mode
 // const serverURL ="http://ec2-18-216-101-119.us-east-2.compute.amazonaws.com:3006";
@@ -36,13 +35,15 @@ const ExpenseTable = ({ open }) => {
   const [user, setUser] = useState({});
   const [expenses, setExpenses] = useState([]);
 
-  onAuthStateChanged(auth, (currUser) => {
-    setUser(currUser);
-  });
+  useEffect(() => {
+    onAuthStateChanged(auth, (currUser) => {
+      setUser(currUser);
+    });
+  }, []);
 
   useEffect(() => {
-      getExpenseReport();
-  }, [user, open]);
+    getExpenseReport();
+  }, [open, user]);
 
   const callAPIGetExpenseReport = async () => {
     console.log("getExpenseReport called");
