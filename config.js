ae9d2022b98e0695b1656423f5c26389da26a516
m227chan <90334226+m227chan@users.noreply.github.com>
2023-05-22 11:58:46 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 
 let config = {
-  host    : 'ec2-18-216-101-119.us-east-2.compute.amazonaws.com',
-  user    : 'm227chan',
-  password: 'u472h46a',
-  database: 'zzammit',
+  host    : '',
+  user    : '',
+  password: '',
+  database: '',
   multipleStatements: true
 };
 
