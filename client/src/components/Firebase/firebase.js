f82fb43d5aa65da860305f5366494fe0fd548bad
m227chan <matthew.tc76@gmail.com>
2023-07-27 20:20:18 -0400

    removed database url

--- client/src/components/Firebase/firebase.js
+++ client/src/components/Firebase/firebase.js
@@ -5,7 +5,6 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: process.env.FB_APIKEY,
   authDomain: process.env.FB_AUTHDOMAIN,
-  databaseURL: process.env.FB_DATABASEURL,
   projectId: process.env.FB_PROJECTID,
   storageBucket: process.env.FB_STORAGEBUCKET,
   messagingSenderId: process.env.FB_MESSAGINGSENDERID,
