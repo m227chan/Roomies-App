a1d28b99371df153140afd296f0ad3d46131bc56
m227chan <90334226+m227chan@users.noreply.github.com>
2023-07-27 21:29:21 -0400

    Update firebase.js

--- client/src/components/Firebase/firebase.js
+++ client/src/components/Firebase/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FB_APIKEY,
-  authDomain: process.env.FB_AUTHDOMAIN,
-  projectId: process.env.FB_PROJECTID,
-  storageBucket: process.env.FB_STORAGEBUCKET,
-  messagingSenderId: process.env.FB_MESSAGINGSENDERID,
-  appId: process.env.FB_APPID,
-  measurementId: process.env.FB_MEASUREMENTID,
+  apiKey: `${process.env.FB_APIKEY}`,
+  authDomain: `${process.env.FB_AUTHDOMAIN}`,
+  projectId: `${process.env.FB_PROJECTID}`,
+  storageBucket: `${process.env.FB_STORAGEBUCKET}`,
+  messagingSenderId: `${process.env.FB_MESSAGINGSENDERID}`,
+  appId: `${process.env.FB_APPID}`,
+  measurementId: `${process.env.FB_MEASUREMENTID}`,
 };
 
 const app = initializeApp(firebaseConfig);
