7eec4de8e45ba094475ada2220243141e908b76d
m227chan <90334226+m227chan@users.noreply.github.com>
2023-07-27 21:30:30 -0400

    Update firebase.js

--- client/src/components/Firebase/firebase.js
+++ client/src/components/Firebase/firebase.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
   appId: `${process.env.FB_APPID}`,
   measurementId: `${process.env.FB_MEASUREMENTID}`,
 };
-
+//test
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
