7a16497f8569246e1cb5203a7f6c40eadcf57743
m227chan <matthew.tc76@gmail.com>
2023-07-27 23:04:50 -0400

    enabled cors

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ let mysql = require("mysql");
 let config = require("./config.js");
 const fetch = require("node-fetch");
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 const bodyParser = require("body-parser");
 
 const { response } = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(cors());
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
